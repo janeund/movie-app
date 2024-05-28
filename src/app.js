43b62d1bbca5ba84df7da9e3794b08f45d3a6aed
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-05-28 13:49:51 +0200

    feat: add basic router

--- src/app.js
+++ src/app.js
@@ -1,2 +1,31 @@
 import "./css/style.css";
 import "./css/reset.css";
+
+const global = {
+  currentPage: window.location.pathname,
+  API_KEY: '005504cfb7e5160f459c7987f1017218'
+}
+
+// Init App
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('home');
+      break;
+    case '/movies.html':
+      console.log('movies');
+      break;
+    case '/series.html':
+      console.log('series');
+      break;
+    case '/people.html':
+      console.log('people');
+      break;
+    case '/search.html':
+      console.log('search');
+      break;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', init);
